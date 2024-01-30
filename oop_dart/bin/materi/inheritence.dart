ed1803426e56df3ad0b1f95f68f407f128110115
ahsani3 <ahsaniweb@gmail.com>
2024-01-30 11:41:34 +0700

    latihan 2 nomor 1

--- oop_dart/bin/materi/inheritence.dart
+++ oop_dart/bin/materi/inheritence.dart
@@ -22,6 +22,56 @@ class Murid extends Orang {
   }
 }
 
+// ============================= Multilevel Inheritance =================================
+class Mobil {
+  // Property
+  String? nama;
+  double? harga;
+}
+
+class Tesla extends Mobil {
+  // Method untuk menampilkan nilai dari property
+  void tampil() {
+    print("Nama: ${nama}");
+    print("Harga: ${harga}");
+  }
+}
+
+class Model3 extends Tesla {
+  // Property
+  String? warna;
+
+  // Method untuk menampilkan nilai dari property
+  void tampil() {
+    super.tampil();
+    print("Warna: ${warna}");
+  }
+}
+
+// ================================ Inheritance Constructor dengan parameter ================================
+
+// Parent Class
+class Binatang {
+  String jenis;
+
+  Binatang(this.jenis);
+
+  void bersuara() {
+    print('Sebuah binatang dari jenis $jenis bersuara.');
+  }
+}
+
+// Child Class yang mewarisi dari Binatang
+class Kucing extends Binatang {
+  String jenisBulu;
+
+  Kucing(String jenis, this.jenisBulu) : super(jenis);
+
+  void tidur() {
+    print('Kucing tidur dengan nyenyak.');
+  }
+}
+
 void main() {
   var murid = Murid();
   murid.nama = "ahsani";
@@ -30,4 +80,13 @@ void main() {
   murid.alamatSekolah = "Jepara";
   murid.tampil();
   murid.tampilInfoSekolah();
+
+  // ======================================== Multilevel Inheritance ========================================
+  Model3 m = new Model3();
+  // memberikan nilai ke object
+  m.nama = "Tesla Model 3";
+  m.harga = 50000.00;
+  m.warna = "Merah";
+  // Menampilkan nilai dari object
+  m.tampil();
 }
